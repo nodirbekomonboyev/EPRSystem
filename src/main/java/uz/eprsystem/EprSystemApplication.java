f37f8073d4e01ee361253d5d12f84c72d4240ed3
nodirbekomonboyev <omonboevnodir77@gmail.com>
2023-10-04 23:14:12 +0500

    Initial commit

---
+++ src/main/java/uz/eprsystem/EprSystemApplication.java
@@ -0,0 +1,13 @@
+package uz.eprsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EprSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EprSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/eprsystem/EprSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.eprsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EprSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

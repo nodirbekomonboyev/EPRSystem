3ed6442b57cf3c18222b654130a295f36ec86ea8
theali <thealiergashev@gmail.com>
2023-10-07 19:49:02 +0500

    change something

--- src/main/java/uz/eprsystem/repository/UserRepository.java
+++ src/main/java/uz/eprsystem/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package uz.eprsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import uz.eprsystem.entity.UserEntity;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findUserEntityByPhoneNumber(String number);
 }
--- src/main/java/uz/eprsystem/service/LessonService.java
+++ src/main/java/uz/eprsystem/service/LessonService.java
@@ -3,11 +3,36 @@ package uz.eprsystem.service;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import uz.eprsystem.entity.Course;
+import uz.eprsystem.entity.LessonEntity;
+import uz.eprsystem.entity.dto.LessonRequestDto;
 import uz.eprsystem.repository.LessonRepository;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class LessonService {
     private final LessonRepository lessonRepository;
     private final ModelMapper modelMapper;
+
+
+
+    public String save(LessonRequestDto dto){
+        LessonEntity map = modelMapper.map(dto, LessonEntity.class);
+        lessonRepository.save(map);
+        return "lesson successfully saved";
+    }
+
+    public List<LessonEntity> getByCourse(Course course){
+        return lessonRepository.getAllByCourse(course);
+    }
+
+
+    public LessonEntity getById(UUID id){
+        return lessonRepository.getById(id);
+    }
+
+
 }
